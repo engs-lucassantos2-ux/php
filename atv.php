96c1594acc57ce2a26fd6fff232968c4231cbd66
Lucas <engs-lucassantos2@camporeal.edu.br>
2026-07-28 21:04:01 -0300

    inicio

---
+++ atv.php
@@ -0,0 +1,26 @@
+<?php
+
+// atividade 1
+$idade = 18;
+$dinheiro = 30;
+
+if ($idade >= 18) {
+    echo "Você é maior de idade\n";
+} else {
+    echo "Você é menor de idade\n";
+}
+
+// atividade 2
+if ($dinheiro < 3) {
+    echo "Pobre";
+} elseif ($dinheiro < 10) {
+    echo "Classe Média";
+} elseif ($dinheiro < 25) {
+    echo "Riquinho";
+} elseif ($dinheiro < 40) {
+    echo "Ricão";
+} else {
+    echo "Elon Musk";
+}
+
+// atividade 3
